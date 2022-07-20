accfd773134715d491dc0091e702b6e4b5f68f4c
Narayan.Singh <euop64vyquancidycnjdydldwyuprii2ukagv47wlzcbwlwkqhfq>
2022-07-20 11:05:59 +0530

    Completed list page UI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TodoListComponent } from "./todo-list/todo-list.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     AppRoutingModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
